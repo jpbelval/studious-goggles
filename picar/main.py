905f2fc33eef16b14577f02b5a0cda3c81d0357c
JP <jp@belval.org>
2025-11-25 15:01:06 -0500

    ADD

--- picar/main.py
+++ picar/main.py
@@ -34,13 +34,13 @@ async def handle_client(websocket):
 
     async def receiver():
         async for message in websocket:
-            engine = json.loads(message)
+            engine = await json.loads(message)
             forward_speed = engine["0"]
             angle = engine["1"]
             print("Speed", engine["0"])
             print("Angle", engine["1"])
-            fw.turn(angle)
-            bw.forward()
+            await fw.turn(angle)
+            await bw.forward()
             bw.speed = forward_speed
 
     send_task = asyncio.create_task(sender())
