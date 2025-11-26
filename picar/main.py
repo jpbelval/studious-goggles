d8a1caef5bcc924ac4f70a5504329b82d029424e
Luka Lepine <117468514+DifficultCupid2@users.noreply.github.com>
2025-11-26 16:58:38 -0500

    Update main.py

--- picar/main.py
+++ picar/main.py
@@ -43,20 +43,14 @@ async def handle_client(websocket):
                 alive.clear()
 
     async def receiver():
+        global latest_message, processing_task
+
         try:
             async for message in websocket:
-                engine = json.loads(message)
-                forward_speed = int(engine["0"])
-                angle = int(engine["1"])
+                latest_message = message
 
-                fw.turn(angle)
-
-                if forward_speed < 0:
-                    bw.backward()
-                    bw.speed = -forward_speed
-                else:
-                    bw.forward()
-                    bw.speed = forward_speed
+                if processing_task is None or processing_task.done():
+                    processing_task = asyncio.create_task(process_latest)
 
         except ConnectionClosed:
             pass
@@ -64,15 +58,37 @@ async def handle_client(websocket):
             alive.clear()
             bw.speed = 0
 
+    async def process_latest():
+        global latest_message
+        message = latest_message
+
+        if message == latest_message:
+            await update_car(message)
+
+    async def update_car(message):
+        engine = json.loads(message)
+        forward_speed = int(engine["0"])
+        angle = int(engine["1"])
+
+        fw.turn(angle)
+
+        if forward_speed < 0:
+            bw.backward()
+            bw.speed = -forward_speed
+        else:
+            bw.forward()
+            bw.speed = forward_speed
+
     send_task = asyncio.create_task(sender())
     recv_task = asyncio.create_task(receiver())
+    process_task = asyncio.create_task(process_latest())
 
     await asyncio.wait(
-        [send_task, recv_task],
-        return_when=asyncio.FIRST_COMPLETED
+        [send_task, recv_task, process_task],
+        return_when=asyncio.ALL_COMPLETED
     )
 
-    for task in (send_task, recv_task):
+    for task in (send_task, recv_task, process_task):
         if not task.done():
             task.cancel()
 
